a841351bbd73bd29a6e9bca984c11eab5df4063f
xp <www.baijuyi@foxmail.com>
2026-01-14 09:30:20 +0800

    Microsoft Clarity

--- src/components/ThirdPartyScripts.tsx
+++ src/components/ThirdPartyScripts.tsx
@@ -75,6 +75,16 @@ export function ThirdPartyScripts () {
 
   return (
     <>
+      {/* Clarity tracking code for https://pixel-beads.com/ */}
+      <Script id="microsoft-clarity" strategy="lazyOnload">
+        {`
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "v12yuvrzjz");
+        `}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-PHKBP63X2W"
         strategy="lazyOnload"
